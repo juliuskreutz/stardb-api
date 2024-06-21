7c3e530b043062aff35f764faecb174a1c9172c9
Julius Kreutz <julius@kreutz.dev>
2024-06-21 18:34:08 +1000

    sitemap

--- src/api/sitemap/mod.rs
+++ src/api/sitemap/mod.rs
@@ -1,4 +1,9 @@
-use actix_web::{get, web, HttpResponse, Responder};
+use std::{
+    sync::Mutex,
+    time::{Duration, Instant},
+};
+
+use actix_web::{get, rt, web, HttpResponse, Responder};
 use serde::Serialize;
 use sqlx::PgPool;
 use strum::IntoEnumIterator;
@@ -8,6 +13,10 @@ use crate::{
     api::{ApiResult, Language},
     database,
 };
+
+lazy_static::lazy_static! {
+    static ref CACHE: Mutex<Option<web::Data<SitemapCache>>> = Mutex::new(None);
+}
 
 #[derive(OpenApi)]
 #[openapi(
@@ -20,8 +29,14 @@ pub fn openapi() -> utoipa::openapi::OpenApi {
     ApiDoc::openapi()
 }
 
-pub fn configure(cfg: &mut web::ServiceConfig) {
-    cfg.service(sitemap);
+pub fn configure(cfg: &mut web::ServiceConfig, pool: PgPool) {
+    let data = CACHE
+        .lock()
+        .unwrap()
+        .get_or_insert_with(|| cache(pool))
+        .clone();
+
+    cfg.service(sitemap).app_data(data);
 }
 
 const LOCALIZED_ROUTES: &[&str] = &[
@@ -39,6 +54,11 @@ const LOCALIZED_ROUTES: &[&str] = &[
     "https://stardb.gg/%LANG%/zzz/achievement-tracker",
     "https://stardb.gg/%LANG%/zzz/signal-tracker",
 ];
+
+#[derive(Default)]
+pub struct SitemapCache {
+    sitemap: futures::lock::Mutex<String>,
+}
 
 #[derive(Serialize)]
 #[serde(rename = "urlset")]
@@ -68,16 +88,39 @@ struct Link {
     href: String,
 }
 
-#[utoipa::path(
-    tag = "sitemap",
-    get,
-    path = "/api/sitemap",
-    responses(
-        (status = 200, description = "Sitemap"),
-    )
-)]
-#[get("/api/sitemap")]
-async fn sitemap(pool: web::Data<PgPool>) -> ApiResult<impl Responder> {
+pub fn cache(pool: PgPool) -> web::Data<SitemapCache> {
+    let sitemap_data = web::Data::new(SitemapCache::default());
+
+    {
+        let sitemap_cache = sitemap_data.clone();
+
+        rt::spawn(async move {
+            let mut interval = rt::time::interval(Duration::from_secs(60 * 60 * 24));
+
+            loop {
+                interval.tick().await;
+
+                let start = Instant::now();
+
+                if let Err(e) = update(&sitemap_cache, &pool).await {
+                    error!(
+                        "Sitemap update failed with {e} in {}s",
+                        start.elapsed().as_secs_f64()
+                    );
+                } else {
+                    info!(
+                        "Sitemap update succeeded in {}s",
+                        start.elapsed().as_secs_f64()
+                    );
+                }
+            }
+        });
+    }
+
+    sitemap_data
+}
+
+async fn update(sitemap_cache: &web::Data<SitemapCache>, pool: &PgPool) -> anyhow::Result<()> {
     let lastmod = "2024-06-21";
 
     let mut urls = Vec::new();
@@ -103,7 +146,7 @@ async fn sitemap(pool: web::Data<PgPool>) -> ApiResult<impl Responder> {
             urls.push(url);
         }
 
-        for id in database::achievements::get_all_ids_shown(&pool).await? {
+        for id in database::achievements::get_all_ids_shown(pool).await? {
             let mut links = Vec::new();
 
             for link_language in Language::iter() {
@@ -123,7 +166,7 @@ async fn sitemap(pool: web::Data<PgPool>) -> ApiResult<impl Responder> {
             urls.push(url);
         }
 
-        for uid in database::mihomo::get_all_uids(&pool).await? {
+        for uid in database::mihomo::get_all_uids(pool).await? {
             for path in ["overview", "characters", "collection"] {
                 let mut links = Vec::new();
 
@@ -152,10 +195,23 @@ async fn sitemap(pool: web::Data<PgPool>) -> ApiResult<impl Responder> {
         url: urls,
     };
 
-    let sitemap = r#"<?xml version="1.0" encoding="UTF-8"?>"#.to_string()
+    *sitemap_cache.sitemap.lock().await = r#"<?xml version="1.0" encoding="UTF-8"?>"#.to_string()
         + &quick_xml::se::to_string(&urlset)?;
 
+    Ok(())
+}
+
+#[utoipa::path(
+    tag = "sitemap",
+    get,
+    path = "/api/sitemap",
+    responses(
+        (status = 200, description = "Sitemap"),
+    )
+)]
+#[get("/api/sitemap")]
+async fn sitemap(sitemap_cache: web::Data<SitemapCache>) -> ApiResult<impl Responder> {
     Ok(HttpResponse::Ok()
         .content_type("application/xml")
-        .body(sitemap))
+        .body(sitemap_cache.sitemap.lock().await.clone()))
 }
