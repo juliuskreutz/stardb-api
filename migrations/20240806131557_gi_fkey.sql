9c21f9431d74fb1d62fcb2cc38d11cd4d7142963
Julius Kreutz <julius@kreutz.dev>
2024-08-06 23:28:50 +1000

    feat: gi fkey

--- migrations/20240806131557_gi_fkey.sql
+++ migrations/20240806131557_gi_fkey.sql
@@ -43,6 +43,30 @@ ALTER TABLE ONLY gi_wishes_weapon
 ALTER TABLE ONLY gi_wishes_chronicled
     ADD CONSTRAINT gi_wishes_chronicled_weapon_fkey FOREIGN KEY (weapon) REFERENCES gi_weapons (id) ON UPDATE CASCADE ON DELETE CASCADE;
 
+ALTER TABLE ONLY gi_wishes_stats_standard
+    ADD CONSTRAINT gi_wishes_stats_standard_uid_fkey FOREIGN KEY (uid) REFERENCES gi_profiles (uid) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_wishes_stats_character
+    ADD CONSTRAINT gi_wishes_stats_character_uid_fkey FOREIGN KEY (uid) REFERENCES gi_profiles (uid) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_wishes_stats_weapon
+    ADD CONSTRAINT gi_wishes_stats_weapon_uid_fkey FOREIGN KEY (uid) REFERENCES gi_profiles (uid) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_wishes_stats_chronicled
+    ADD CONSTRAINT gi_wishes_stats_chronicled_uid_fkey FOREIGN KEY (uid) REFERENCES gi_profiles (uid) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_wishes_stats_global_standard
+    ADD CONSTRAINT gi_wishes_stats_global_standard_uid_fkey FOREIGN KEY (uid) REFERENCES gi_profiles (uid) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_wishes_stats_global_character
+    ADD CONSTRAINT gi_wishes_stats_global_character_uid_fkey FOREIGN KEY (uid) REFERENCES gi_profiles (uid) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_wishes_stats_global_weapon
+    ADD CONSTRAINT gi_wishes_stats_global_weapon_uid_fkey FOREIGN KEY (uid) REFERENCES gi_profiles (uid) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_wishes_stats_global_chronicled
+    ADD CONSTRAINT gi_wishes_stats_global_chronicled_uid_fkey FOREIGN KEY (uid) REFERENCES gi_profiles (uid) ON UPDATE CASCADE ON DELETE CASCADE;
+
 ALTER TABLE ONLY gi_characters_text
     ADD CONSTRAINT gi_characters_text_character_fkey FOREIGN KEY (id) REFERENCES gi_characters (id) ON UPDATE CASCADE ON DELETE CASCADE;
 
