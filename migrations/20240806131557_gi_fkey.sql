0e35af480c834f5e13cd1a6c9ecd63308ba2afab
Julius Kreutz <julius@kreutz.dev>
2024-08-06 23:24:29 +1000

    feat: gi fkey

---
+++ migrations/20240806131557_gi_fkey.sql
@@ -0,0 +1,51 @@
+ALTER TABLE ONLY gi_wishes_beginner
+    ADD CONSTRAINT gi_wishes_beginner_uid_fkey FOREIGN KEY (uid) REFERENCES gi_profiles (uid) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_wishes_standard
+    ADD CONSTRAINT gi_wishes_standard_uid_fkey FOREIGN KEY (uid) REFERENCES gi_profiles (uid) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_wishes_character
+    ADD CONSTRAINT gi_wishes_character_uid_fkey FOREIGN KEY (uid) REFERENCES gi_profiles (uid) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_wishes_weapon
+    ADD CONSTRAINT gi_wishes_weapon_uid_fkey FOREIGN KEY (uid) REFERENCES gi_profiles (uid) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_wishes_chronicled
+    ADD CONSTRAINT gi_wishes_chronicled_uid_fkey FOREIGN KEY (uid) REFERENCES gi_profiles (uid) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_wishes_beginner
+    ADD CONSTRAINT gi_wishes_beginner_character_fkey FOREIGN KEY (character) REFERENCES gi_characters (id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_wishes_standard
+    ADD CONSTRAINT gi_wishes_standard_character_fkey FOREIGN KEY (character) REFERENCES gi_characters (id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_wishes_character
+    ADD CONSTRAINT gi_wishes_character_character_fkey FOREIGN KEY (character) REFERENCES gi_characters (id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_wishes_weapon
+    ADD CONSTRAINT gi_wishes_weapon_character_fkey FOREIGN KEY (character) REFERENCES gi_characters (id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_wishes_chronicled
+    ADD CONSTRAINT gi_wishes_chronicled_character_fkey FOREIGN KEY (character) REFERENCES gi_characters (id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_wishes_beginner
+    ADD CONSTRAINT gi_wishes_beginner_weapon_fkey FOREIGN KEY (weapon) REFERENCES gi_weapons (id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_wishes_standard
+    ADD CONSTRAINT gi_wishes_standard_weapon_fkey FOREIGN KEY (weapon) REFERENCES gi_weapons (id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_wishes_character
+    ADD CONSTRAINT gi_wishes_character_weapon_fkey FOREIGN KEY (weapon) REFERENCES gi_weapons (id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_wishes_weapon
+    ADD CONSTRAINT gi_wishes_weapon_weapon_fkey FOREIGN KEY (weapon) REFERENCES gi_weapons (id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_wishes_chronicled
+    ADD CONSTRAINT gi_wishes_chronicled_weapon_fkey FOREIGN KEY (weapon) REFERENCES gi_weapons (id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_characters_text
+    ADD CONSTRAINT gi_characters_text_character_fkey FOREIGN KEY (id) REFERENCES gi_characters (id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY gi_weapons_text
+    ADD CONSTRAINT gi_weapons_text_weapon_fkey FOREIGN KEY (id) REFERENCES gi_weapons (id) ON UPDATE CASCADE ON DELETE CASCADE;
+
