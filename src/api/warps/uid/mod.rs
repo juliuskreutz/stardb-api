4472b905ce79d7b6104a2a93453480eeb78bd6cd
Julius Kreutz <julius@kreutz.dev>
2024-08-08 21:52:23 +1000

    feat: better warps api

--- src/api/warps/uid/mod.rs
+++ src/api/warps/uid/mod.rs
@@ -1,24 +1,31 @@
 use actix_session::Session;
 use actix_web::{get, web, HttpResponse, Responder};
 use chrono::{DateTime, Utc};
-use serde::{Deserialize, Serialize};
 use sqlx::PgPool;
-use utoipa::{IntoParams, OpenApi, ToSchema};
+use utoipa::OpenApi;
 
 use crate::{
     api::{ApiResult, LanguageParams},
-    database, GachaType,
+    database,
 };
 
 #[derive(OpenApi)]
 #[openapi(
     tags((name = "warps/{uid}")),
     paths(get_warps),
-    components(schemas(Warp, WarpType))
+    components(schemas(Warps, Warp, WarpType))
 )]
 struct ApiDoc;
 
-#[derive(Serialize, ToSchema)]
+#[derive(serde::Serialize, utoipa::ToSchema)]
+struct Warps {
+    departure: Vec<Warp>,
+    standard: Vec<Warp>,
+    character: Vec<Warp>,
+    light_cone: Vec<Warp>,
+}
+
+#[derive(serde::Serialize, utoipa::ToSchema)]
 struct Warp {
     r#type: WarpType,
     id: String,
@@ -47,16 +54,11 @@ impl From<database::warps::DbWarp> for Warp {
     }
 }
 
-#[derive(Serialize, ToSchema)]
+#[derive(serde::Serialize, utoipa::ToSchema)]
 #[serde(rename_all = "snake_case")]
 enum WarpType {
     Character,
     LightCone,
-}
-
-#[derive(Deserialize, IntoParams)]
-struct WarpParams {
-    gacha_type: GachaType,
 }
 
 pub fn openapi() -> utoipa::openapi::OpenApi {
@@ -71,9 +73,9 @@ pub fn configure(cfg: &mut web::ServiceConfig) {
     tag = "warps/{uid}",
     get,
     path = "/api/warps/{uid}",
-    params(LanguageParams, WarpParams),
+    params(LanguageParams),
     responses(
-        (status = 200, description = "[Warp]", body = Vec<Warp>),
+        (status = 200, description = "Warps", body = Warps),
     )
 )]
 #[get("/api/warps/{uid}")]
@@ -81,7 +83,6 @@ async fn get_warps(
     session: Session,
     uid: web::Path<i32>,
     language_params: web::Query<LanguageParams>,
-    warp_params: web::Query<WarpParams>,
     pool: web::Data<PgPool>,
 ) -> ApiResult<impl Responder> {
     let uid = *uid;
@@ -107,16 +108,33 @@ async fn get_warps(
 
     let language = language_params.lang;
 
-    let db_warps = match warp_params.gacha_type {
-        GachaType::Departure => {
-            database::warps::departure::get_by_uid(uid, language, &pool).await?
-        }
-        GachaType::Standard => database::warps::standard::get_by_uid(uid, language, &pool).await?,
-        GachaType::Special => database::warps::special::get_by_uid(uid, language, &pool).await?,
-        GachaType::Lc => database::warps::lc::get_by_uid(uid, language, &pool).await?,
-    };
+    let departure = database::warps::departure::get_by_uid(uid, language, &pool)
+        .await?
+        .into_iter()
+        .map(Warp::from)
+        .collect();
+    let standard = database::warps::standard::get_by_uid(uid, language, &pool)
+        .await?
+        .into_iter()
+        .map(Warp::from)
+        .collect();
+    let character = database::warps::special::get_by_uid(uid, language, &pool)
+        .await?
+        .into_iter()
+        .map(Warp::from)
+        .collect();
+    let light_cone = database::warps::lc::get_by_uid(uid, language, &pool)
+        .await?
+        .into_iter()
+        .map(Warp::from)
+        .collect();
 
-    let warps: Vec<_> = db_warps.into_iter().map(Warp::from).collect();
+    let warps = Warps {
+        departure,
+        standard,
+        character,
+        light_cone,
+    };
 
     Ok(HttpResponse::Ok().json(warps))
 }
