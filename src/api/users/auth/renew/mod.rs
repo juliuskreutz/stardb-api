febf0c338c3d64234b5ec22ac89bd76191da6db6
Julius Kreutz <julius@kreutz.dev>
2024-11-15 12:08:28 +0100

    fix: api path

--- src/api/users/auth/renew/mod.rs
+++ src/api/users/auth/renew/mod.rs
@@ -6,7 +6,7 @@ use crate::api::ApiResult;
 
 #[derive(OpenApi)]
 #[openapi(
-    tags((name = "users/me/renew")),
+    tags((name = "users/auth/renew")),
     paths(post_renew)
 )]
 struct ApiDoc;
@@ -20,15 +20,15 @@ pub fn configure(cfg: &mut web::ServiceConfig) {
 }
 
 #[utoipa::path(
-    tag = "users/me/renew",
+    tag = "users/auth/renew",
     get,
-    path = "/api/users/me/renew",
+    path = "/api/users/auth/renew",
     responses(
         (status = 200, description = "Successfully renewed", body = String),
         (status = 400, description = "Not logged in"),
     )
 )]
-#[post("/api/users/me/renew")]
+#[post("/api/users/auth/renew")]
 async fn post_renew(session: Session) -> ApiResult<impl Responder> {
     let Ok(Some(username)) = session.get::<String>("username") else {
         return Ok(HttpResponse::BadRequest().finish());
