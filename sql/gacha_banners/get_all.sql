49b3cb8a0b7354b7cf3e3478040283711cbcf442
Js41637 <js41637@gmail.com>
2025-06-21 14:38:54 +1000

    feat: new generic and unified banner system

---
+++ sql/gacha_banners/get_all.sql
@@ -0,0 +1,5 @@
+SELECT
+    *
+FROM
+    gacha_banners;
+
--- src/api/admin/mod.rs
+++ src/api/admin/mod.rs
@@ -1,6 +1,7 @@
 mod delete_unofficial_signals;
 mod delete_unofficial_warps;
 mod delete_unofficial_wishes;
+mod gacha_banners;
 
 use actix_web::web;
 
@@ -8,11 +9,13 @@ pub fn openapi() -> utoipa::openapi::OpenApi {
     let mut openapi = delete_unofficial_signals::openapi();
     openapi.merge(delete_unofficial_warps::openapi());
     openapi.merge(delete_unofficial_wishes::openapi());
+    openapi.merge(gacha_banners::openapi());
     openapi
 }
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.configure(delete_unofficial_signals::configure)
         .configure(delete_unofficial_warps::configure)
-        .configure(delete_unofficial_wishes::configure);
+        .configure(delete_unofficial_wishes::configure)
+        .configure(gacha_banners::configure);
 }
