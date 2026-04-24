b7d13ab03b4ab039d3d01543266207fb97b546ab
Js41637 <js41637@gmail.com>
2026-04-25 00:04:40 +1000

    fix: updating banners api

--- sql/banners/set.sql
+++ sql/banners/set.sql
@@ -1,3 +1,9 @@
 INSERT INTO banners (id, name, start, "end", character, light_cone)
-    VALUES ($1, $2, $3, $4, $5, $6);
-
+    VALUES ($1, $2, $3, $4, $5, $6)
+ON CONFLICT (id)
+    DO UPDATE SET
+        name = excluded.name,
+        start = excluded.start,
+        "end" = excluded."end",
+        character = excluded.character,
+        light_cone = excluded.light_cone;
--- sql/gi/banners/set.sql
+++ sql/gi/banners/set.sql
@@ -1,3 +1,9 @@
 INSERT INTO gi_banners (id, name, start, "end", character, weapon)
-    VALUES ($1, $2, $3, $4, $5, $6);
-
+    VALUES ($1, $2, $3, $4, $5, $6)
+ON CONFLICT (id)
+    DO UPDATE SET
+        name = excluded.name,
+        start = excluded.start,
+        "end" = excluded."end",
+        character = excluded.character,
+        weapon = excluded.weapon;
