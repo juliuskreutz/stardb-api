bbe45c208b350386fa6b634e20927cce699ab5eb
Hashblen <62646051+hashblen@users.noreply.github.com>
2026-07-14 16:30:23 +0200

    Feat: multi-game UIGF import

--- src/api/gi/mod.rs
+++ src/api/gi/mod.rs
@@ -1,7 +1,6 @@
 mod achievements;
 mod banners;
 mod paimon_wishes_import;
-mod uigf_wishes_import;
 mod wishes;
 mod wishes_import;
 
@@ -17,7 +16,6 @@ pub fn openapi() -> utoipa::openapi::OpenApi {
     openapi.merge(achievements::openapi());
     openapi.merge(banners::openapi());
     openapi.merge(paimon_wishes_import::openapi());
-    openapi.merge(uigf_wishes_import::openapi());
     openapi.merge(wishes::openapi());
     openapi.merge(wishes_import::openapi());
     openapi
@@ -27,7 +25,6 @@ pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.configure(achievements::configure)
         .configure(banners::configure)
         .configure(paimon_wishes_import::configure)
-        .configure(uigf_wishes_import::configure)
         .configure(wishes::configure)
         .configure(wishes_import::configure);
 }
